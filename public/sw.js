6dcff7c45ce2f135fc59ec61447d1291b5674d49
Stark <sobanalif22@nutech.edu.pk>
2026-08-13 22:13:11 +0500

    fix: insert missing closing div tag to close first-boot-wizard overlay before pos-app-layout

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,7 @@ if (self.location.hostname !== 'localhost' && !self.location.hostname.includes('
 }
 
 const urlParams = new URLSearchParams(self.location.search);
-const buildVersion = urlParams.get('v') || 'v2.6.0-prod-wiz-before-layout-v1';
+const buildVersion = urlParams.get('v') || 'v2.6.0-prod-closed-wizard-v1';
 const CACHE_NAME = `valenixia-pos-cache-${buildVersion}`;
 const ASSETS_TO_CACHE = [
   { url: '/', integrity: '' },
